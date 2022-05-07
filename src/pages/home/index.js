bc0ff2348d961d75948c8d743327da2eef765e4c
kim <yuyukgh@naver.com>
2022-05-07 17:12:21 +0900

    [Feat] home section S01 components update
    - css bouns animation 
    - Do not hover animaiton

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,8 +1,21 @@
 /** @jsxImportSource @emotion/react */
-import { test } from "./style";
+import { container, box, title, desc, sectionDown } from "./style";
+
+import arrow_down_icon from "../../assets/images/home/arrow_down_icon.png";
 
 const Home = () => {
-  return <div css={test}>home...</div>;
+  return (
+    <section css={container}>
+      <div css={box}>
+        <h2 css={title}>ADVENTURE FRAME</h2>
+        <p css={desc}>We make what you EXACTLY need</p>
+      </div>
+      <div css={sectionDown}>
+        <p>Let’s explore</p>
+        <img src={arrow_down_icon} alt="" />
+      </div>
+    </section>
+  );
 };
 
 export default Home;
--- src/pages/home/style.js
+++ src/pages/home/style.js
@@ -1,7 +1,50 @@
 import { css } from "@emotion/react";
-import { FontsatoshiBold } from "../../assets/styles/fonts";
+import { Fontanton, FontsatoshiBold, FontsatoshiRegular } from "../../assets/styles/fonts";
+import { bounce } from "../../assets/styles/animaition";
 
-export const test = css`
-  font-size: 1.25rem;
+export const container = css`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  background-color: #f7f7f7;
+  position: relative;
+  z-index: -1;
+`;
+
+export const box = css`
+  text-align: center;
+  cursor: pointer;
+`;
+
+export const title = css`
+  font-size: 10vw;
+  letter-spacing: 1rem;
+  color: #000;
+  ${Fontanton}
+`;
+
+export const desc = css`
+  font-size: 1.5rem;
+  color: #000;
   ${FontsatoshiBold}
+`;
+
+export const sectionDown = css`
+  position: absolute;
+  bottom: 2rem;
+  text-align: center;
+  transform: translate(-50%, 0);
+  animation: ${bounce} 1s ease infinite;
+
+  > p {
+    font-size: 1.125rem;
+    color: #000;
+    padding-bottom: 1rem;
+    ${FontsatoshiRegular}
+  }
+  > img {
+    width: 100%;
+    max-width: 1.25rem;
+  }
 `;
